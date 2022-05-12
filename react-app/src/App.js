d26aa6d2a21614e85c7de44de9f7aab80168a870
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-05-12 00:02:38 -0500

    Added bootstrap and navbar with our colors

---
+++ react-app/src/App.js
@@ -0,0 +1,24 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Container} from "react-bootstrap";
+import BookNavbar from "./components/BookNavbar";
+import BooksRow from "./components/BooksRow";
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            {/* 
+        Navbar
+        Main (bookcards)
+        Footer
+      */}
+            <BookNavbar />
+            <Container>
+                <BooksRow title="Nonfiction Best Sellers" />
+            </Container>
+        </div>
+    );
+}
+
+export default App;
