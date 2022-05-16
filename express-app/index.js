27012b4f9c885f827e6c9024e848c6fe83228579
unknown <waldoch.don@gmail.com>
2022-05-16 14:25:15 -0500

    Started adding routes

--- express-app/index.js
+++ express-app/index.js
@@ -1,3 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch-commonjs");
+
 const { FetchBooks, FetchClubs, FetchUsers, FetchRosters } =  require("./queries/index.js")
 
 const FetchAllBooks = async  () => {
@@ -17,10 +22,31 @@ const FetchAllRosters = async  () => {
 
 const FetchAllUsers = async  () => {
   const users = await FetchUsers()
-  console.log('Users:\n', users)
+  return(users)
 }
 
-FetchAllBooks()
-FetchAllClubs()
-FetchAllRosters()
-FetchAllUsers()
+app.get("/", async (req, res) => {
+  res.status(200).json({
+      message: "Welcome to our Bookclub API",
+  });
+});
+
+app.get("/Users", async (req, res) => {
+  const response = await FetchAllUsers();
+  res.status(200).json(response);
+});
+
+app.get("/Users/:email", async (req, res) => {
+  console.log(req.params.email)
+  const response = await FetchAllUsers();
+  res.status(200).json(response);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on PORT: ${process.env.PORT}`);
+});
+
+// FetchAllBooks()
+// FetchAllClubs()
+// FetchAllRosters()
+// FetchAllUsers()
