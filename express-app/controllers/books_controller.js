4eaae498a31c64834a59913fdceb3bdfeeeeaf0f
unknown <waldoch.don@gmail.com>
2022-05-16 22:04:44 -0500

    Implemented Controllers

--- express-app/controllers/books_controller.js
+++ express-app/controllers/books_controller.js
@@ -1,7 +1,21 @@
 const { db } = require("../dbConnection");
-const express = require('express')
-const books = express.Router()
+const express = require('express');
+const books = express.Router();
 
+///////////////////////////////////////////////////////////////////////
+// CRUD Routes
+
+// Create Routes
+books.post("/", async (req, res) => {
+  try {
+    const response = await queryToInsertBooks(req.body);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Read Routes
 books.get("/", async (req, res) => {
   const response = await queryToFetchBooks();
   res.status(200).json(response);
@@ -12,40 +26,23 @@ books.get("/:isbn", async (req, res) => {
   res.status(200).json(response);
 });
 
-books.post("/", async (req, res) => {
-  try {
-    const response = await queryToInsertBooks(req.body);
-    res.status(200).json(response);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+///////////////////////////////////////////////////////////////////////
+// Queries
 
 queryToFetchBooks = (isbn) => {
-    var query = (isbn === undefined) ? `SELECT * FROM books` : `SELECT * FROM books WHERE isbn = '${isbn}'`
+    var query = (isbn === undefined) ? `SELECT * FROM books` : `SELECT * FROM books WHERE isbn = '${isbn}'`;
     return db.manyOrNone(
       `${query}`
-    )
+    );
 }
 
 queryToInsertBooks = (body) => {
   console.log(body)
   return db.one(
-    `INSERT 
-      INTO
-      books (
-        isbn,
-        title,
-        author,
-        description
-      )
-      VALUES (
-        $/isbn/,
-        $/title/,
-        $/author/,
-        $/description/
-      )
-      RETURNING isbn
+    `
+    INSERT INTO books (isbn, title, author, description)
+    VALUES ($/isbn/, $/title/, $/author/, $/description/)
+    RETURNING isbn
     `,
     { ...body }
   );
